6db1553aa78de0e90227cae139355fb59a94568a
TareqBhuiyan <taru4951@gmail.com>
2021-07-13 00:00:56 +0600

    LAB-1

--- login.php
+++ login.php
@@ -1,41 +1,55 @@
 <?php
-//session_start();
-$uname="";
-$err_uname="";
-$pass="";
-$err_pass="";
-
-$hasError=false;
-
-$users = array ("tareq" =>"1234", "sabbir"=>"1234","karim"=> "1234","rahim"=>"1234");
-
-if($_SERVER["REQUEST_METHOD"]== "POST"){
-	
-	if (empty($_POST["uname"])) {
-		$err_uname= "Username Required";
-		$hasError = true;
-	}
-	 else{
-	      $uname=$_POST["uname"];
-	 }
-	 if (empty($_POST["pass"])){
-		 $err_pass = " Password required ";
-		 $hasError = true ;
-	 }
-	 else {
-		 $pass = $_POST["pass"];
-	 }
-	 if (!hasError) {
-		 foreach ($users as $u => $u ) {
-			 if ($uname == $u && $pass==$p){
-				 //$_SESSION ["loggeduser"] = $uname;
-				 setcookie ("loggeduser",$uname,time ()+120);
-			 header ("Location: dasboard.php");
-			 
-			 }
-		 }
-		 echo "Invalid user";
-	 }
-}
+        //session_start();
+        $uname="";
+        $err_uname="";
+        $pass="";
+        $err_pass="";
+       
+        $hasError=false;
+       
+        $users = array("tareq"=>"1234","sabbir"=>"123","karim"=>"456","rahim"=>"789");
+       
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+                if(empty($_POST["uname"]))
+                {
+                    $hasError=true;
+                    $err_uname="Username Required";
+                }
+                else
+                        {
+                            $uname=$_POST["uname"];
+                        }
+                       
+                if(empty($_POST["pass"]))
+                {
+                    $hasError=true;
+                    $err_pass="Password Required";
+                }
+                else
+                        {
+                            $pass=$_POST["pass"];
+                        }
+                       
+                if(!$hasError){
+                    foreach($users as $u=>$p){
+                        if($uname == $u && $pass == $p){
+                            //$_SESSION["loggeduser"] = $uname;
+                            setcookie("loggeduser",$uname,time()+120);
+                            header("Location: dashboard.php");
+                        }
+                    }
+                    echo "Invalid username";
+                }
+        }
 ?>
-	
+<html>
+    <body>
+        <form action="" method="post">
+            Username: <input type="text" name="uname" value="<?php echo $uname;?>"><br>
+             <span><?php echo $err_uname; ?></span><br>
+            Password: <input type="password" name="pass" value="<?php echo $pass;?>"><br>
+             <span><?php echo $err_pass; ?></span><br>
+            <input type="submit" value="Login">
+        </form>
+    </body>
+</html>
